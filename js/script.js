2d355ab7c818b1239dcc61353f5d9cd423addb41
Vinizin05 <89863696+Vinizin05@users.noreply.github.com>
2022-06-29 13:55:13 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-import countdown from "./countdown.js";
-const temp = new countdown("13 july 2022 20:00:00 GMT-0300")
-console.log(temp.segundos);
-                                                       
+import Countdown from "./countdown.js";
+
+const tempoParaOWWB = new Countdown("13 August 2022 20:00:00 GMT-0300");
+const tempos = document.querySelectorAll("[data-time]");
+
+function mostrarTempo() {
+  tempos.forEach((tempo, index) => {
+    tempo.innerHTML = tempoParaOWWB.total[index];
+  });
+}
+mostrarTempo();
+setInterval(mostrarTempo, 1000);
